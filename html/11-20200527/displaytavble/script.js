9847847ca2a4aa3eb197da5adea7c181bb6762cf
Ngọc Minh <48076532+minhnguyen120898@users.noreply.github.com>
2020-05-28 01:16:25 +0700

    btvnss11

--- html/11-20200527/displaytavble/script.js
+++ html/11-20200527/displaytavble/script.js
@@ -12,20 +12,26 @@ var obj = {
 
 var keys = Object.keys(obj);
 console.log(keys);
-
+var values = Object.values(obj);
+console.log(values[0].STT);
 function diengiatri(){
     console.log("Dien gia tri!");
-    for(var i=0;i<2 ;i++){
+    for(var i=0; i<2; i++){
         var row = document.createElement("tr");
-        
-        for(var j=0; j < 3 ; j++){
-            var col = document.createElement("td");
-            var txt = document.createTextNode("row" + i + "col" +j);
-            col.appendChild(txt);
-            row.appendChild(col);
-        }
-
         document.getElementById("data").appendChild(row);
+
+        var col1 = document.createElement("td");
+        col1.innerHTML = values[i].STT;
+
+        var col2 = document.createElement("td");
+        col2.innerHTML = keys[i];
+        
+        var col3 = document.createElement("td");
+        col3.innerHTML = values[i].gia;
+        
+        row.appendChild(col1);
+        row.appendChild(col2);
+        row.appendChild(col3);
     }
     
 }
